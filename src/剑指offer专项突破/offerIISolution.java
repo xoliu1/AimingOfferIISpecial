32210693faa608de5e16edfe3e4518286e1315f7
xoliu1 <xoliu6@gmail.com>
2023-11-16 22:44:50 +0800

    1116.02

--- src/剑指offer专项突破/offerIISolution.java
+++ src/剑指offer专项突破/offerIISolution.java
@@ -717,6 +717,26 @@ class offer {
     }
 
 
+    /**
+     * LCR 033. 字母异位词分组
+     * @param strs
+     * @return List<List<String>>
+     * @author xoliu
+     * @create 2023/11/16 22:29
+     **/
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
 
 
     /**
